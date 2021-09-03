8353445a5376240e888b22992ded03295ef6b6f3
Michael Rieder <1219037+miried@users.noreply.github.com>
2021-09-03 18:35:36 +0200

    New try

--- Library.fs
+++ Library.fs
@@ -1,12 +1,12 @@
 ﻿module NativeLibrary
 open System.Runtime.InteropServices
 open System
-     
-[<NativeCallable(EntryPoint = "add", CallingConvention = CallingConvention.Cdecl)>]
+
+[<UnmanagedCallersOnly(EntryPoint = "add")>]
 let Add (a: int) (b: int) : int =
     a + b
     
-[<NativeCallable(EntryPoint = "write_line", CallingConvention = CallingConvention.Cdecl)>]
+[<UnmanagedCallersOnly(EntryPoint = "write_line")>]
 let WriteLine (pString: IntPtr) : int =
     try
         let str = Marshal.PtrToStringAnsi(pString)
